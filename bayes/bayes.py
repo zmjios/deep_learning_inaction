cd179c1ae0a8586fbfc9da9a91afeaa88a767c1a
mjzeng <mjzeng@ctrip.com>
2017-11-12 12:20:04 +0800

    决策树

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -40,24 +40,25 @@ def bagOfWord2VecMN(vocabList, inputSet):
 
 
 def trainNB0(trainMatrix, trainCategory):
-    numTrainDoc = len(trainMatrix)
-    numWords = len(trainMatrix[0])
-    pAbusive = sum(trainCategory) / float(numTrainDoc)
+    numTrainDoc = len(trainMatrix)  # 计算训练的文档数目
+    numWords = len(trainMatrix[0])  # 计算每篇文档的词条数
+    pAbusive = sum(trainCategory) / float(numTrainDoc)  # 文档属于侮辱类的概率
     p0Num = ones(numWords)
     p1Num = ones(numWords)
     p0Denom = 2.0
     p1Denom = 2.0
     for i in range(numTrainDoc):
+        # 统计属于侮辱类的条件概率所需的数据，即P(w0|1),P(w1|1),P(w2|1)···
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
             p1Denom += sum(trainMatrix[i])
-        else:
+        else:  # 统计属于非侮辱类的条件概率所需的数据，即P(w0|0),P(w1|0),P(w2|0)···
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
     print("p0Denom = %f" % p0Denom)
     print("p1Denom = %f" % p1Denom)
-    p1Vect = p1Num / p1Denom
-    p0Vect = p0Num / p0Denom
+    p1Vect = log(p1Num / p1Denom)
+    p0Vect = log(p0Num / p0Denom)
     return p0Vect, p1Vect, pAbusive
 
 
